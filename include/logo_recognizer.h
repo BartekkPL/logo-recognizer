027e27da42026ed4923d562fbbcdbe6adb3524ae
Bartłomiej Kunikowski <bartekk.nie@gmail.com>
2018-05-23 20:25:36 +0200

    Adds contrast and brightness change functions

--- include/logo_recognizer.h
+++ include/logo_recognizer.h
@@ -13,6 +13,8 @@ class LogoRecognizer {
   ~LogoRecognizer() = default;
   bool recognizeLogo(cv::Mat& image);
  private:
+  void changeBrightness(cv::Mat& image, int brightness);
+  void changeContrast(cv::Mat& image, int contrast);
   void tresholding(cv::Mat& image, cv::Scalar low_bgr, cv::Scalar high_bgr);
 };
 
--- src/logo_recognizer.cc
+++ src/logo_recognizer.cc
@@ -4,10 +4,41 @@ bool LogoRecognizer::recognizeLogo(cv::Mat& image) {
   if (image.empty())
     return false;
 
-  tresholding(image, cv::Scalar(0, 0, 128), cv::Scalar(100, 100, 255));
+  changeContrast(image, 150);
+  tresholding(image, cv::Scalar(0, 0, 128), cv::Scalar(128, 128, 255));
   return true;
 }
 
+void LogoRecognizer::changeBrightness(cv::Mat& image, int brightness) {
+  cv::Mat_<cv::Vec3b> x = image;
+  for (int i = 0; i < x.rows; i++) {
+    for (int j = 0; j < x.cols; j++) {
+      for (int k = 0; k < 3; k++) {
+        int color = x(i, j)[k] + brightness;
+        x(i, j)[k] = color > 255 ? 255 : color < 0 ? 0 : color;
+      }
+    }
+  }
+  image = x;
+}
+
+/** Algorithm come from
+ * http://www.dfstudios.co.uk/articles/programming/image-programming-algorithms/
+ * image-processing-algorithms-part-5-contrast-adjustment/ webpage
+ */
+void LogoRecognizer::changeContrast(cv::Mat& image, int contrast) {
+  float contrastFactor = (259 * (contrast + 255)) / (255 * (259 - contrast));
+  cv::Mat_<cv::Vec3b> x = image;
+  for (int i = 0; i < x.rows; i++) {
+    for (int j = 0; j < x.cols; j++) {
+      for (int k = 0; k < 3; k++) {
+        int color = contrastFactor * (x(i, j)[k] - 128) + 128;
+        x(i, j)[k] = color > 255 ? 255 : color < 0 ? 0 : color;
+      }
+    }
+  }
+  image = x;
+}
 
 void LogoRecognizer::tresholding(cv::Mat& image, cv::Scalar low_bgr,
                                  cv::Scalar high_bgr) {
